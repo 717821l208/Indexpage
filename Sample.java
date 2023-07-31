a7fbda55ba52de0aeba0f9b8613e712811029267
Dhanu <136722100+717821l208@users.noreply.github.com>
2023-07-31 20:22:09 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,28 @@
+Public class mainc{
+	Public static void main (string[]args)
+	{
+	int i;
+	int flag=0;
+	intn = integer.parseint(args[1];
+	if(n== 0) (n==1)
+	flag=1;
+	for(i=2;i<=n/2;++i)
+	{
+	if(n%i==0)
+	{
+	flag=1;
+	Break;
+	}
+	}
+	if(flag==0)
+	system.out.println("prime Numbers");
+			system.out.println("Not a prime Number");
+	}
+	
+  }
+
+}
+
+OUTPUT:
+        1
+
